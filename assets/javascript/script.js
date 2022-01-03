924669204661c0eb5f3c6878da8435ac97c06dda
cleave13 <94774869+cleave13@users.noreply.github.com>
2022-01-02 19:21:54 -0700

    Added comments to all code

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,12 +1,14 @@
+// Creates constant variables for the hamburger and nav-menu CSS classes
 const hamburger = document.querySelector('.hamburger');
 console.log(hamburger);
 const navMenu = document.querySelector('.nav-menu');
 
+//Listens for the menu to be clicked and toggles the active .class
 hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('active');
     navMenu.classList.toggle('active');
 })
-
+//Removes the .active class when an item is selected from the nav menu or the hamburger is clicked again
 document.querySelectorAll('.nav-link').forEach(n => n.addEventListener('click', () => {
     hamburger.classList.remove('active');
     navMenu.classList.remove('active');
